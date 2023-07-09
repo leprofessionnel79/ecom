aa0d3aaaf8d055b8b8af2c58452fa3398cd877fd
leprofessionnel79 <jannblomondo@gmail.com>
2023-07-09 12:33:03 +0300

    change side bar icons

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -21,16 +21,7 @@
   <link rel="shortcut icon" href="{{asset('admin/images/favicon.png')}}" />
 
 
-  {{-- <link rel="stylesheet" href="vendors/mdi/css/materialdesignicons.min.css">
-  <link rel="stylesheet" href="vendors/base/vendor.bundle.base.css">
-  <!-- endinject -->
-  <!-- plugin css for this page -->
-  <link rel="stylesheet" href="vendors/datatables.net-bs4/dataTables.bootstrap4.css">
-  <!-- End plugin css for this page -->
-  <!-- inject:css -->
-  <link rel="stylesheet" href="css/style.css">
-  <!-- endinject -->
-  <link rel="shortcut icon" href="images/favicon.png" /> --}}
+ 
     @livewireStyles
 </head>
 <body>
--- resources/views/layouts/inc/admin/sidebar.blade.php
+++ resources/views/layouts/inc/admin/sidebar.blade.php
@@ -7,9 +7,15 @@
         </a>
       </li>
       <li class="nav-item">
+        <a class="nav-link" href="index.html">
+          <i class="mdi mdi-sale menu-icon"></i>
+          <span class="menu-title">Sales</span>
+        </a>
+      </li>
+      <li class="nav-item">
         <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-          <i class="mdi mdi-circle-outline menu-icon"></i>
-          <span class="menu-title">UI Elements</span>
+          <i class="mdi  mdi-view-list menu-icon"></i>
+          <span class="menu-title">Category</span>
           <i class="menu-arrow"></i>
         </a>
         <div class="collapse" id="ui-basic">
@@ -20,30 +26,56 @@
         </div>
       </li>
       <li class="nav-item">
+        <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic1" aria-expanded="false" aria-controls="ui-basic1">
+          <i class="mdi  mdi-plus-circle menu-icon"></i>
+          <span class="menu-title">Products</span>
+          <i class="menu-arrow"></i>
+        </a>
+        <div class="collapse" id="ui-basic1">
+          <ul class="nav flex-column sub-menu">
+            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Buttons</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Typography</a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item">
         <a class="nav-link" href="pages/forms/basic_elements.html">
           <i class="mdi mdi-view-headline menu-icon"></i>
-          <span class="menu-title">Form elements</span>
+          <span class="menu-title">Brands</span>
         </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic2" aria-expanded="false" aria-controls="ui-basic2">
+          <i class="mdi mdi-account-supervisor menu-icon"></i>
+          <span class="menu-title">Users</span>
+          <i class="menu-arrow"></i>
+        </a>
+        <div class="collapse" id="ui-basic2">
+          <ul class="nav flex-column sub-menu">
+            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Buttons</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Typography</a></li>
+          </ul>
+        </div>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="pages/charts/chartjs.html">
           <i class="mdi mdi-chart-pie menu-icon"></i>
-          <span class="menu-title">Charts</span>
+          <span class="menu-title">Home Slider</span>
         </a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="pages/tables/basic-table.html">
-          <i class="mdi mdi-grid-large menu-icon"></i>
-          <span class="menu-title">Tables</span>
+          <i class="mdi mdi-settings menu-icon"></i>
+          <span class="menu-title">Site Setting</span>
         </a>
       </li>
-      <li class="nav-item">
+      {{-- <li class="nav-item">
         <a class="nav-link" href="pages/icons/mdi.html">
           <i class="mdi mdi-emoticon menu-icon"></i>
           <span class="menu-title">Icons</span>
         </a>
-      </li>
-      <li class="nav-item">
+      </li> --}}
+      {{-- <li class="nav-item">
         <a class="nav-link" data-bs-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
           <i class="mdi mdi-account menu-icon"></i>
           <span class="menu-title">User Pages</span>
@@ -58,12 +90,12 @@
             <li class="nav-item"> <a class="nav-link" href="pages/samples/lock-screen.html"> Lockscreen </a></li>
           </ul>
         </div>
-      </li>
-      <li class="nav-item">
+      </li> --}}
+      {{-- <li class="nav-item">
         <a class="nav-link" href="documentation/documentation.html">
           <i class="mdi mdi-file-document-box-outline menu-icon"></i>
           <span class="menu-title">Documentation</span>
         </a>
-      </li>
+      </li> --}}
     </ul>
   </nav>
