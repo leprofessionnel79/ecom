093a0fd3b99106dd70fff0cb10510a67aee1a887
leprofessionnel79 <jannblomondo@gmail.com>
2023-08-21 20:11:38 +0300

    add remove to wishlist

--- app/Http/Livewire/Frontend/WishlistShow.php
+++ app/Http/Livewire/Frontend/WishlistShow.php
@@ -8,6 +8,16 @@ use App\Models\Wishlist;
 
 class WishlistShow extends Component
 {
+    public function removeWishlistItem(int $wishlistId)
+    {
+        Wishlist::where('user_id',auth()->user()->id)->where('id',$wishlistId)->delete();
+        $this->dispatchBrowserEvent('message',[
+           'text'=>'WishList Item Removed Successfully',
+           'type'=> 'success',
+           'status'=> 200
+        ]);
+    }
+
     public function render()
     {
         $wishlist = Wishlist::where('user_id',auth()->user()->id)->get();
