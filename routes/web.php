b39b0e0fb1d9b905c67ba2c745be6ed3156ae32d
leprofessionnel79 <jannblomondo@gmail.com>
2023-12-20 14:46:29 +0300

    add password change feature

--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,10 @@ Route::middleware(['auth'])->group(function () {
     Route::get('orders',[App\Http\Controllers\Frontend\OrderController::class,'index']);
 
     Route::get('orders/{oredreId}',[App\Http\Controllers\Frontend\OrderController::class,'show']);
+    Route::get('profile',[App\Http\Controllers\Frontend\UserController::class,'index']);
+    Route::post('profile',[App\Http\Controllers\Frontend\UserController::class,'updateUserDetails']);
+    Route::get('change-password',[App\Http\Controllers\Frontend\UserController::class,'passwordCreate']);
+    Route::post('change-password',[App\Http\Controllers\Frontend\UserController::class,'changePassword']);
 
 });
 
