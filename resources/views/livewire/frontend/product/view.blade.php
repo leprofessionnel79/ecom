758749c3209986875c6b86d3f06c798992f7d3f7
leprofessionnel79 <jannblomondo@gmail.com>
2024-02-14 19:13:08 +0300

    update carosuel

--- resources/views/livewire/frontend/product/view.blade.php
+++ resources/views/livewire/frontend/product/view.blade.php
@@ -240,24 +240,25 @@
     $(window).on('load',function(){
         $('.four-carousel').owlCarousel({
         loop:true,
-        margin:10,
+        margin:7,
         dots:true,
         nav:false,
+        responsiveClass:true,
         autoplay:true,
         autoplayHoverPause:true,
         autoPlayTimeout:1000,
         smartSpeed:1000,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:3
-            },
-            1000:{
-                items:4
-            }
-        }
+        // responsive:{
+        //     // 0:{
+        //     //     items:3
+        //     // },
+        //     // 600:{
+        //     //     items:3
+        //     // },
+        //     // 1000:{
+        //     //     items:4
+        //     // }
+        // },
     });
 });
 </script>
