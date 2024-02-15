820fd732b85f2b6c8dc019dba73516ac9a52058d
leprofessionnel79 <jannblomondo@gmail.com>
2024-02-15 22:10:41 +0300

    updated margin

--- resources/views/auth/passwords/confirm.blade.php
+++ resources/views/auth/passwords/confirm.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container mt-5">
-    <div class="row justify-content-center">
+<div class="container mb-5 mt-5">
+    <div class="row justify-content-center" style="margin-top: 9%">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Confirm Password') }}</div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container mb-5 mt-5">
-    <div class="row justify-content-center">
+    <div class="row justify-content-center" style="margin-top: 9%">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Register') }}</div>
